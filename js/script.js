706c95ad0677dedc2ed931ff3293e3e5a2e43186
Vara <varachkin@gmail.com>
2020-10-17 17:21:35 +0300

    working

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ let nameEl = document.getElementById('inputName');
 let passwordEl = document.getElementById('inputPassword');
 let emailEl = document.getElementById('inputEmail');
 let viewEl = document.getElementById('view');
+let submitEl = document.getElementById('submit');
+let nameError = document.getElementsByClassName('erName');
+let passwordError = document.getElementsByClassName('erPassword');
+let emailError = document.getElementsByClassName('erEmail');
 
 viewEl.addEventListener("click", showPassword);
 
@@ -14,8 +18,22 @@ function showPassword(){
         passwordEl.type = 'password';
     }
 }
-function validateName(name){
-    for (let i = 0; i < name.length; i++){
-
+function validateNull(even) {
+    even.preventDefault();
+    if (!nameEl.value){
+        nameEl.style.border = "1 solid red";
+        nameError.style.opacity = "1";
+        // alert('no value entered Name');
+    }
+    if (!passwordEl.value){
+        passwordEl.style.border = "1 solid red";
+        passwordError.style.opacity = "1";
+        // alert('no value entered Password')
+    }
+    if (!emailEl.value){
+        emailEl.style.border = "1 solid red";
+        emailError.style.opacity = "1";
+        // alert('no value entered E-mail')
     }
 }
+submitEl.addEventListener("click", validateNull);
